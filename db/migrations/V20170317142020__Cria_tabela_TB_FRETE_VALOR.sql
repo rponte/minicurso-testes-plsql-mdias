d84d1e3578d29a6743396f01b2a84f07cc9cbc71
Rafael Ponte <rponte@gmail.com>
2017-03-17 14:08:56 -0300

    limpando projeto - removendo artefatos do blank

---
+++ db/migrations/V20170317142020__Cria_tabela_TB_FRETE_VALOR.sql
@@ -0,0 +1,11 @@
+-- Create table
+create table TB_FRETE_VALOR
+(
+  ID              INTEGER      NOT NULL,
+  UF_DESTINO      VARCHAR2(2)  NOT NULL,
+  VALOR           NUMBER(14,2) NOT NULL
+);
+
+alter table TB_FRETE_VALOR
+  add constraint TB_FRETE_VALOR_PK primary key (ID);
+);
