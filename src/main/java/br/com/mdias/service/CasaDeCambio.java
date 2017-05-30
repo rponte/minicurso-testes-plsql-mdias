c2d2cb7c04c9a063e6b35adac494b6ad8838a190
Rafael Ponte <rponte@gmail.com>
2017-05-30 17:09:11 -0300

    corrige codigo e testes;

--- src/main/java/br/com/mdias/service/CasaDeCambio.java
+++ src/main/java/br/com/mdias/service/CasaDeCambio.java
@@ -4,12 +4,12 @@ public class CasaDeCambio {
 
 	public double converteRealParaBitcoin(double valorEmReal) {
 		
-		return valorEmReal * 9200;
+		return valorEmReal / 9200;
 	}
 	
 	public double converteRealParaDolar(double valorEmReal) {
 		
-		return valorEmReal * 3.55;
+		return valorEmReal / 3.55;
 	}
 	
 }
--- src/test/java/br/com/mdias/service/CasaDeCambioTest.java
+++ src/test/java/br/com/mdias/service/CasaDeCambioTest.java
@@ -9,27 +9,27 @@ public class CasaDeCambioTest {
 	@Test
 	public void deveConverterValorEmRealParaBitcoin() {
 		// cenário
-		double valorEmReal = 1;
+		double valorEmReal = 9200;
 		
 		// ação
 		CasaDeCambio cambio = new CasaDeCambio();
 		double valorEmBitcoin = cambio.converteRealParaBitcoin(valorEmReal);
 		
 		// validação
-		assertEquals(9200, valorEmBitcoin, 0.001);
+		assertEquals(1, valorEmBitcoin, 0.001);
 	}
 	
 	@Test
 	public void deveConverterValorEmRealParaDolar() {
 		// cenário
-		double valorEmReal = 1;
+		double valorEmReal = 3.55;
 
 		// ação
 		CasaDeCambio cambio = new CasaDeCambio();
 		double valorEmDolar = cambio.converteRealParaDolar(valorEmReal);
 
 		// validação
-		assertEquals(3.55, valorEmDolar, 0.001);
+		assertEquals(1, valorEmDolar, 0.001);
 	}
 
 }
